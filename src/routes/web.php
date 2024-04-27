0b6918b34a0ce03efcd0b3564a107ed313fa14a0
fukuda <sfu983667@gmail.com>
2024-04-27 10:51:09 +0900

    2-8

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Models\Person;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,23 @@ use App\Http\Controllers\AuthController;
 
 Route::middleware('auth')->group(function () {
     Route::get('/', [AuthController::class, 'index']);
+});
+
+Route::get('/softdelete', function () {
+    Person::find(1)->delete();
+});
+
+Route::get('softdelete/get', function() {
+    $person = Person::onlyTrashed()->get();
+    dd($person);
+});
+
+Route::get('softdelete/store', function() {
+    $result = Person::onlyTrashed()->restore();
+    echo $result;
+});
+
+Route::get('softdelete/absolute', function() {
+    $result = Person::onlyTrashed()->forceDelete();
+    echo $result;
 });
